3516cf82db3c933ce8373cbe54ca078a7d342adc
Nefedov Sergey <nefedov38@gmail.com>
2024-09-30 13:42:36 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import { routes } from "./routes/routes";
+import Footer from "./components/Footer.jsx";
 function App() {
   return (
     <>
@@ -11,7 +12,7 @@ function App() {
           <Route path={r.path} element={r.element} key={r.path} />
         ))}
       </Routes>
-      <footer>footer</footer>
+      <Footer/>
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,39 @@
-const Footer = () => (
+const Footer = () =>
+    <div style={{height:"100px",width:"100%",position:"absolute",bottom:0,marginTop:"10px"}}>
+        <div  className="bg-gray-800">
+            <div>
+                <div>
+                    <a href="tel:88001234567">88001234567</a>
+                </div>
+                <div>
+                    Online консультант
+                </div>
+            </div>
+            <div>
+                <div>
+                    <a href="tel:88001234567">88001234567</a>
+                </div>
+                <div>
+                    Интернет-магазин
+                </div>
+            </div>
+        </div>
+        <div className="bg-gray-900" style={{height:"100px"}}>
+            Салоны
+            Каталог
+            События
+            Журнал
+            Контакты
+            Мы в социальных сетях:
+            Условия доставки
+            Возврат товара
+            Дисконтная программа
+            Оплата и политика конфиденциальности
+        </div>
+
+    </div>
+
     
-);
+;
 
 export default Footer;
