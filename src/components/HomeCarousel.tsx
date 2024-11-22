a557825bf5ac38cb5ffb6877401e36fc5c5e6947
Nefedov Sergey <nefedov38@gmail.com>
2024-11-22 11:39:10 +0300

    carousel

---
+++ src/components/HomeCarousel.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Carousel} from "react-bootstrap";
+
+const HomeCarousel = () => {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <div>
+                    <img src="/carousel/arteOlfato.jpg" alt="arteOlfato"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/culti.jpg" alt="culti"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/mdci.jpg" alt="mdci"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/nayassia.jpg" alt="nayassia"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/oroGold.jpg" alt="oroGold"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/mdci2.jpg" alt="mdci"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/panouge.jpg" alt="panouge"/>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div>
+
+                    <img src="/carousel/perfumeCult.jpg" alt="perfumeCult"/>
+                </div>
+            </Carousel.Item>
+        </Carousel>
+
+
+    );
+}
+
+
+export default HomeCarousel;
