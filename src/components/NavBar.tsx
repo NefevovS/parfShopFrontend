4cd492453ef20a11bea46eaad7813d4bfe3ea1db
Nefedov Sergey <nefedov38@gmail.com>
2024-11-28 17:10:28 +0300

    refactoring navigation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,27 +24,31 @@ const NavBar = () => {
                             <Link className="nav-link text-white" to="/contacts">Контакты</Link>
                         </li>
                     </ul>
+                    <div className="d-flex gap-3">
 
-                    {isAuth?(<ul className="navbar-nav d-flex gap-3 ">
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" aria-current="page" to="/admin" >Админ</Link>
-                        </li>
-                        <li className="nav-item wrapper">
+                        <div className="nav-item wrapper">
                             <Link aria-current="page" to="/cart" ><img src="/cart.svg" height={"40px"} width={"40px"}/>
-
                             </Link>
                             <span className="text-wrap text-center text-white lh-1" >2</span>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/auth">Выйти</Link>
-                        </li>
-                    </ul>):(<ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" aria-current="page" to="/auth" >Зарегистрироваться</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white" to="/auth">Войти</Link>
-                        </li></ul>)}
+                        </div>
+
+                        {isAuth?(<ul className="navbar-nav d-flex gap-3 ">
+                            <li className="nav-item">
+                                <Link className="nav-link text-white d-block bg-danger rounded p-2" aria-current="page" to="/admin" >Админ</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/auth">Выйти</Link>
+                            </li>
+                        </ul>):(<ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" aria-current="page" to="/auth" >Зарегистрироваться</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white" to="/auth">Войти</Link>
+                            </li></ul>)}
+                    </div>
+
+
 
                 </div>
             </div>
